b27cfcfb00d3e78ea0e36390c306aeb688a9c976
tashirokana <kana.tashiro@rakus-partners.co.jp>
2022-05-10 13:42:13 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleTashiroApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleTashiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleTashiroApplication.class, args);
+	}
+
+}
